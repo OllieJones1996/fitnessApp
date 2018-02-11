3bf634ee6728183c9e4ae2263e15983da82d3d91
Ollie Jones <catchemolly@hotmail.co.uk>
2018-02-11 18:48:33 +0000

    Added Google sign in functionality

--- MainActivity.java
+++ MainActivity.java
@@ -6,18 +6,29 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.auth.api.Auth;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-    private SignInButton sign_in_button;
+    private SignInButton mSignInbutton;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private static int RC_SIGN_IN = 9001;
+    public String toast = "DEBUG";
 
 
 
@@ -48,6 +59,55 @@ public class MainActivity extends AppCompatActivity {
             s.setVisibility(s.VISIBLE);
     }
 
+
+    /////////////////////////////////////////////////////////////////
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null)
+            updateUI(account);
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+            Toast.makeText(this, toast, Toast.LENGTH_LONG).show();
+        } catch (ApiException e) {
+
+            //Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        String email = account.getDisplayName();
+        mTextMessage.setText(email);
+
+    }
+
+    //////////////////////////////////////////////////////////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,14 +117,26 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        sign_in_button = (SignInButton) findViewById(R.id.sign_in_button);
+        mSignInbutton = (SignInButton) findViewById(R.id.sign_in_button);
 
-        sign_in_button.setOnClickListener(new View.OnClickListener() {
+        //////////GOOGLE SIGN IN/////////////
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        mSignInbutton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                buttonSwitchVisibility(sign_in_button);
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.sign_in_button:
+                        signIn(); //call the signin method
+                        break;
+                }
             }
         });
+        /////////////////////////////////////
 
     }
 
