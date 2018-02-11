cce8ccff26fb9db429cdab91fff24d99f181f109
Ollie Jones <catchemolly@hotmail.co.uk>
2018-02-11 14:57:56 +0000

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.ollie.fitnessapp;
+
+import android.content.Intent;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private SignInButton sign_in_button;
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    public void buttonSwitchVisibility(SignInButton s){
+        if (s.getVisibility() == s.VISIBLE)
+            s.setVisibility(s.INVISIBLE);
+        else
+            s.setVisibility(s.VISIBLE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        sign_in_button = (SignInButton) findViewById(R.id.sign_in_button);
+
+        sign_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonSwitchVisibility(sign_in_button);
+            }
+        });
+
+    }
+
+}
