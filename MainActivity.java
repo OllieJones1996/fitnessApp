7eef7ef2016c8dea406f7792b6529fed5cbd99d0
Ollie Jones <catchemolly@hotmail.co.uk>
2018-02-12 00:34:53 +0000

    Added subscribing and unsubscribing to the Google fit API

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ollie.fitnessapp;
 
+import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
@@ -20,17 +21,34 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.FitnessStatusCodes;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Subscription;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.auth.api.Auth;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
     private SignInButton mSignInbutton;
     private GoogleSignInClient mGoogleSignInClient;
+    private GoogleApiClient mGoogleApiClient;
 
     private static int RC_SIGN_IN = 9001;
     public String toast = "DEBUG";
+
+    public static final String TAG = "BasicRecordingApi";
+
+    private static final int REQUEST_OAUTH_REQUEST_CODE = 1;
 
 
 
@@ -46,13 +64,18 @@ public class MainActivity extends AppCompatActivity {
                     android.support.v4.app.FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
                     fragmentTransaction1.replace(R.id.frameLayout, fragment, "homeFragment");
                     fragmentTransaction1.commit();
+
+                    subscribe();
                     return true;
+
                 case R.id.action_graph:
                     setTitle("GRAPH");
                     graphFragment fragment2 = new graphFragment();
                     android.support.v4.app.FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
                     fragmentTransaction2.replace(R.id.frameLayout, fragment2, "graphFragment");
                     fragmentTransaction2.commit();
+
+                    cancelSubscription();
                     return true;
 
             }
@@ -67,8 +90,106 @@ public class MainActivity extends AppCompatActivity {
             s.setVisibility(s.VISIBLE);
     }
 
+    ////////////////////////////////////////////////////////////////
+    private void listSubs(){
+        Fitness.getRecordingClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .listSubscriptions(DataType.TYPE_ACTIVITY_SAMPLES)
+                .addOnSuccessListener(new OnSuccessListener<List<Subscription>>() {
+                    @Override
+                    public void onSuccess(List<Subscription> subscriptions) {
+                        for (Subscription sc : subscriptions) {
+                            DataType dt = sc.getDataType();
+                            Log.i(TAG, "Active subscription for data type: " + dt.getName());
+                        }
+                    }
+                });
+    }
 
-    /////////////////////////////////////////////////////////////////
+    public void subscribe() {
+        // To create a subscription, invoke the Recording API. As soon as the subscription is
+        // active, fitness data will start recording.
+        // [START subscribe_to_datatype]
+        Fitness.getRecordingClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .subscribe(DataType.TYPE_ACTIVITY_SAMPLES)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.i(TAG, "Successfully subscribed!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i(TAG, "There was a problem subscribing.");
+                    }
+                });
+        // [END subscribe_to_datatype]
+    }
+
+    //@Override
+    protected void onActivityResultFitness(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_OAUTH_REQUEST_CODE) {
+                subscribe();
+            }
+        }
+    }
+
+    /**
+     * Fetches a list of all active subscriptions and log it. Since the logger for this sample
+     * also prints to the screen, we can see what is happening in this way.
+     */
+    private void dumpSubscriptionsList() {
+        // [START list_current_subscriptions]
+        Fitness.getRecordingClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .listSubscriptions(DataType.TYPE_ACTIVITY_SAMPLES)
+                .addOnSuccessListener(new OnSuccessListener<List<Subscription>>() {
+                    @Override
+                    public void onSuccess(List<Subscription> subscriptions) {
+                        for (Subscription sc : subscriptions) {
+                            DataType dt = sc.getDataType();
+                            Log.i(TAG, "Active subscription for data type: " + dt.getName());
+                        }
+                    }
+                });
+        // [END list_current_subscriptions]
+    }
+
+    /**
+     * Cancels the ACTIVITY_SAMPLE subscription by calling unsubscribe on that {@link DataType}.
+     */
+    private void cancelSubscription() {
+        final String dataTypeStr = DataType.TYPE_ACTIVITY_SAMPLES.toString();
+        Log.i(TAG, "Unsubscribing from data type: " + dataTypeStr);
+
+        // Invoke the Recording API to unsubscribe from the data type and specify a callback that
+        // will check the result.
+        // [START unsubscribe_from_datatype]
+        Fitness.getRecordingClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .unsubscribe(DataType.TYPE_ACTIVITY_SAMPLES)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.i(TAG, "Successfully unsubscribed for data type: " + dataTypeStr);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Subscription not removed
+                        Log.i(TAG, "Failed to unsubscribe for data type: " + dataTypeStr);
+                    }
+                });
+        // [END unsubscribe_from_datatype]
+    }
+
+    ///////////////////////////////////////////////////////////
+
+
+
+
+
+    /////////////////////GOOGLE SIGN IN////////////////////////////////////////////
     @Override
     protected void onStart() {
         super.onStart();
@@ -115,6 +236,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //////////////////////////////////////////////////////////////////
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -143,11 +267,30 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        /////////////////////////////////////
 
-        ///on start up home fragment shown///
+        FitnessOptions fitnessOptions = FitnessOptions.builder().addDataType(DataType.TYPE_ACTIVITY_SAMPLES).build();
+        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                    this,
+                    REQUEST_OAUTH_REQUEST_CODE,
+                    GoogleSignIn.getLastSignedInAccount(this),
+                    fitnessOptions);
+        } else {
+            subscribe();
+        }
 
 
+       /* if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                    this,
+                    REQUEST_OAUTH_REQUEST_CODE,
+                    GoogleSignIn.getLastSignedInAccount(this),
+                    fitnessOptions);
+        } else {
+            subscribe();
+        }
+    }
+    */
 
     }
 
